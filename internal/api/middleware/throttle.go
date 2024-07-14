e68a8bbeedfe6f2bc0e2857451f750ada8923331
Komeil Shadan <kshadannejad@gmail.com>
2024-07-14 17:00:43 +0330

    update rate limiter to use mutex locks for race conditions

--- internal/api/middleware/throttle.go
+++ internal/api/middleware/throttle.go
@@ -9,18 +9,28 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 	"time"
+)
+
+var (
+	mutex             sync.Mutex
+	rateLimitDuration = 15 * time.Minute
+	apiQuota          = os.Getenv("API_QUOTA")
 )
 
 func Throttle(rdb *redis.Client) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		// everytime a user queries, check if the IP is already in database,
 		// if yes, decrement the calls remaining by one, else add the IP to database
-		// with expiry of `30mins`. So in this case the user will be able to send 10
-		// requests every 30 minutes
+		// with expiry of `15mins`. So in this case the user will be able to send 10
+		// requests every 15 minutes
+		mutex.Lock()
+		defer mutex.Unlock()
+
 		val, err := rdb.Get(rdb.Context(), ctx.ClientIP()).Result()
 		if errors.Is(err, redis.Nil) {
-			err = rdb.Set(rdb.Context(), ctx.ClientIP(), os.Getenv("API_QUOTA"), 30*time.Minute).Err()
+			err = rdb.Set(rdb.Context(), ctx.ClientIP(), apiQuota, rateLimitDuration).Err()
 			if err != nil {
 				log.Error(log.Redis, log.Insert, err, nil)
 				ctx.AbortWithStatus(http.StatusInternalServerError)
@@ -36,8 +46,8 @@ func Throttle(rdb *redis.Client) gin.HandlerFunc {
 			if intval <= 0 {
 				limit, _ := rdb.TTL(rdb.Context(), ctx.ClientIP()).Result()
 
-				ctx.AbortWithStatusJSON(http.StatusForbidden, AppHttp.ApiResponse{
-					Message: "Forbidden",
+				ctx.AbortWithStatusJSON(http.StatusTooManyRequests, AppHttp.ApiResponse{
+					Message: "Too Many Requests",
 					Error:   errors.New("rate limit exceeded").Error(),
 					Data: map[string]any{
 						"rate_limit_reset": limit / time.Nanosecond / time.Minute,
