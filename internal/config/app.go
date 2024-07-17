714106f8cd36b2954f3d810f4ca7d07e5a1980cb
Komeil Shadan <kshadannejad@gmail.com>
2024-07-17 20:52:56 +0330

    avoid duplicate records on mongo and add base url for shortlinks

--- internal/config/app.go
+++ internal/config/app.go
@@ -10,10 +10,11 @@ const (
 )
 
 type App struct {
-	Host        string `env:"APP_HOST" envDefault:"localhost"`
-	Port        int    `env:"APP_PORT" envDefault:"8080"`
-	Key         string `env:"APP_KEY" envDefault:""`
-	Mode        string `env:"APP_MODE" envDefault:"debug"`
-	Environment Env    `env:"APP_ENV" envDefault:"local"`
-	APIQuota    string `env:"API_QUOTA" envDefault:"10"`
+	Host             string `env:"APP_HOST" envDefault:"localhost"`
+	Port             int    `env:"APP_PORT" envDefault:"8080"`
+	Key              string `env:"APP_KEY" envDefault:""`
+	Mode             string `env:"APP_MODE" envDefault:"debug"`
+	Environment      Env    `env:"APP_ENV" envDefault:"local"`
+	APIQuota         string `env:"API_QUOTA" envDefault:"10"`
+	ShortLinkBaseURL string `env:"SHORT_LINK_BASE_URL" envDefault:"https://drto-link.com/"`
 }
